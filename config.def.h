930424a1e733ea75d3544b27261c8bd0db70316e
liqlvnvn <liqlvnvn@gmail.com>
2021-01-30 19:42:13 +0300

    Initial commit

--- config.def.h
+++ config.def.h
@@ -44,7 +44,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+// #define MODKEY Mod1Mask  // Alt key
+#define MODKEY Mod4Mask     // Win key
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
